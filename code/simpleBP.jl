3a8b5395879c61a6018cda2ea1dee3f0254e0ee4
James Watmough <watmough@unb.ca>
2022-07-03 09:39:48 -0300

    wrap simple BP script in function

--- code/simpleBP.jl
+++ code/simpleBP.jl
@@ -3,13 +3,30 @@ using StatsBase
 # parameters and distributions
 offspringdist = weights([.1 .75 .1 .05])
 
-# initialize matrix for results
-population = zeros(Int,30,20)
-population[1,:].=1
+function BPsimple1(duration, samples)
+	# initialize matrix for results
+	population = zeros(Int,duration,samples)
+	population[1,:].=1
 
-for s in 1:size(population)[2]
-	for k in 1:(size(population)[1]-1) 
-		population[k+1,s] = sum(sample(collect(0:length(offspringdist)-1),offspringdist ,population[k,s];replace=true))
+	for s in 1:size(population)[2]
+		for k in 1:(size(population)[1]-1) 
+			population[k+1,s] = sum(sample(collect(0:length(offspringdist)-1),offspringdist ,population[k,s];replace=true))
+		end
 	end
+
+	return(population)
 end
 
+# rewrite using broadcasting for inner loop
+# surprisingly only slightly faster
+function BPsimple2(duration, samples)
+	# initialize matrix for results
+	population = zeros(Int,samples,duration)
+	population[:,1].=1
+	for k in 1:(size(population)[2]-1) 
+		population[:,k+1] = 
+			[sum(sample(collect(0:length(offspringdist)-1),offspringdist ,population[s,k];replace=true)) 
+			 for s in 1:size(population)[1] ]
+	end
+	return(population)
+end
